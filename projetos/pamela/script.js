b27c1f14abcd3785530cabbfd9fc5a48bfb95dd2
Pedro Mariano <126967446+PheagaMariano@users.noreply.github.com>
2023-11-02 13:47:31 -0300

    update

--- projetos/pamela/script.js
+++ projetos/pamela/script.js
@@ -5,10 +5,10 @@ document.querySelector("form").addEventListener("submit", (e) => {
 
   function obterMensagem(n) {
     if (n < 0 || n > 50 || isNaN(n)) return "Macaca do Carai, O Numero Deve Ser De 0 A 50!";
-    if (n == 0) return "VSF MACACA, eu vou chorar se ficar sabendo que vc colocou 0, Pamy!"
+    if (n == 0) return "0?? Não sabo se vc acha isso mesmo, pedro macaco triste..."
     if (n <= 10) return "Menos de 11????, Pedro macaco triste";
     if (n <= 20) return "Isso é muito pouco Pamy crl, vc tem mais uma chance";
-    if (n <= 30) return ", Pamy";
+    if (n <= 30) return "Eu sei que vc consegue acertar, Pamy, tenta de novo!!";
     if (n <= 49) return "Passou perto, mas ainda não é isso!!!!";
     return "Acertou! Que Fofo kkkkk, mas claramente eu ti gosto mt mais que isso fedorenta, sinto sua falta!";
   }
